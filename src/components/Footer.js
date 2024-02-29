553d27a50d3c3788d0d49c06d5702001dbb9ac38
Egnuma Gelana <101870281+IdentityG@users.noreply.github.com>
2024-02-29 07:27:37 -0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ const Footer = () => {
     <footer className=" bg-black py-8 text-center text-white">
       <div className=" container mx-auto">
         {" "}
-        <div> Copyright & copy; 2023. All rights reserved. </div>{" "}
-        <div>masudfcs1 </div>{" "}
+        <div> Copyright & copy; 2024. All rights reserved. </div>{" "}
+        <div>Egnuma Gelana </div>{" "}
       </div>{" "}
     </footer>
   );
